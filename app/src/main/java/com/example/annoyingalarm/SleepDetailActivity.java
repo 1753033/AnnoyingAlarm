5b93f4eab0c657f58430419d51e1736fdfc36232
Binh Mai <1753030@student.hcmus.edu.vn>
2020-05-09 09:32:07 +0700

    Sleep Details Activity

---
+++ app/src/main/java/com/example/annoyingalarm/SleepDetailActivity.java
@@ -0,0 +1,14 @@
+package com.example.annoyingalarm;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SleepDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sleep_detail);
+    }
+}
